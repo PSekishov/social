59fb361dd1cc97115b1acf295836e3341b16a523
Pavel Sekishov <pasha-sveta@yandex.ru>
2021-12-17 19:58:28 +0200

    Refactoring +
    Component  <Contacts />

---
+++ src/components/Contacts/Contacts.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import './contacts.css';
+
+const Contacts = () => {
+
+
+    return (
+        <>
+            <h5 className="text-muted">Contacts</h5>
+
+            <div className="card mb-3">
+                <div className="card-body">IMG+IND Daniel Ricciardo</div>
+            </div> 
+
+            <div className="card mb-3">
+                <div className="card-body">IMG+IND Max Sekishov</div>
+            </div>
+
+            <div className="card mb-3">
+                <div className="card-body">IMG+IND Margarita Sekishova</div>
+            </div>              
+                
+        </>
+    );
+};
+
+export default Contacts;
--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -9,6 +9,7 @@ import Header from '../Header';
 import Footer from '../Footer';
 import MapSite from '../MapSite';
 import Profile from '../Profile';
+import Contacts from '../Contacts';
 
 
 const Layout = () => {
@@ -27,39 +28,19 @@ const Layout = () => {
                     <div className="row">
 
                         <div className="col-md-3">
-   
                             <Profile />
                             <MapSite />
-
                         </div>
 
                         <div className="col-md-6">
-                        <Outlet />
+                            <Outlet />
                         </div>
 
                         <div className="col-md-3">
-                            <h5 className="text-muted">Contacts</h5>
-                            <div className="card mb-3">
-                                <div className="card-body">
-                                    IMG+IND Daniel Ricciardo
-                                </div>
-                            </div>
-
-                            <div className="card mb-3">
-                                <div className="card-body">
-                                IMG+IND Max Sekishov
-                                </div>
-                            </div>
-
-                            <div className="card mb-3">
-                                <div className="card-body">
-                                IMG+IND Margarita Sekishova
-                                </div>
-                            </div>
-
+                            <Contacts />
                         </div>
 
-                    </div>
+                    </div> 
                 </div> 
             </main> 
             </div>   {/* wrapper */}
